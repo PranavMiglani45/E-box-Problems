e9f96d3518423994dd51320e1f25ddccbdf52f2e
Pranav Miglani <72353081+PranavMiglani45@users.noreply.github.com>
2021-09-01 23:02:41 +0530

    Create Shape.java

---
+++ Object Oriented Programming using Java Review II - Programming/Area of shape/Shape.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Shape
+{
+    protected  Double area;
+    
+    public Shape()
+    {
+    }
+    public Shape(Double area)
+    {
+        this.area=area;
+    }
+    
+    public void setArea(Double area)
+    {
+        this.area=area;
+    }
+    public Double getArea()
+    {
+        return this.area;
+    }
+    
+    public void computeArea()
+    {
+       this.area=(double)0;
+    }
+}
