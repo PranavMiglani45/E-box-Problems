7f328a800249cc63ecafa4c34544137be72b73ca
Pranav Miglani <72353081+PranavMiglani45@users.noreply.github.com>
2021-09-03 23:44:10 +0530

    Create Event.java

---
+++ Object Oriented Programming using Java Review II - Programming/Overriding-simple/Event.java
@@ -0,0 +1,16 @@
+public class Event{
+    protected String name;
+    protected String detail;
+    protected String ownerName;
+    
+    public Event(){}
+    public Event(String name,String detail,String ownerName){
+        this.name=name;
+        this.detail=detail;
+        this.ownerName=ownerName;
+    }
+    
+}
+    
+    
+    
